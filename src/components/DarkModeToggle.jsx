6d176be49322cea6c53119d23bd3559785bff05c
Adrian Moring <adriantmoringbass@gmail.com>
2025-10-24 23:17:36 -0400

    passes tests

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/DarkMode.module.css";
+import { useState } from "react";
 
 const DarkModeToggle = () => {
-  // TODO: Implement dark mode toggle logic
+  // Tests expect the app to start in dark mode, so default to true
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
-    <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
-  )
-}
+    <div>
+      <button
+        onClick={toggleTheme}
+        className={isDarkMode ? styles["button-light"] : styles["button-dark"]}
+      >
+        Toggle {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </div>
+  );
+};
 
-export default DarkModeToggle
+export default DarkModeToggle;
